16b0009f62df62735b140bb1162a6f294c981362
Farid <houseraidar@gmail.com>
2016-01-12 16:17:58 +0100

    -Schönere Buttons -Hintergrundmusik

--- Projekt Spieleentwickung/src/Gui.java
+++ Projekt Spieleentwickung/src/Gui.java
@@ -3,127 +3,175 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Sound;
 import org.newdawn.slick.geom.Circle;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Shape;
 
 public class Gui {
-	
-	 private int status = 0;  // 0 = Hauptmenü; 1 = im Spiel
-		
-		private int x;
-	    private int hoehe = 50;
-	    private int breite = 120;
-		
-	    Image[] bild;
-	  
-	    
-		Shape[] buttons;
 
-		
-		public void render(GameContainer container, Graphics g) throws SlickException {
-			
-			if (status == 1) {
-				
+	private int status = 0;  // 0 = Hauptmenü; 1 = im Spiel
+
+	private int xButton;
+	private int hoeheButton = 50;
+	private int breiteButton = 120;
+
+	Image[] bild;
+
+	Image hintergrundbild;
+	private int hoeheBild;
+
+	private Sound hintergrundmusik;
+	private Sound click;
+
+	Shape[] buttons;
+
+
+	public void render(GameContainer container, Graphics g) throws SlickException {
+
+		if (status == 1) {
+
 			g.setColor(Color.white);
 			g.texture(buttons[2], bild[2], true);
-			
+
+		}
+
+		if(status == 0){
+
+			hintergrundbild.drawCentered(container.getWidth()/2, container.getHeight()/2);
+
+			g.setColor(Color.red);
+			g.drawString("Spielmenü", xButton, 50);
+
+			for (int i = 0; i < buttons.length-1; i++) {
+				g.setColor(Color.white);
+				g.texture(buttons[i], bild[i], true);
 			}
-			if(status == 0){
-				g.setColor(Color.red);
-				g.drawString("Spielmenü", x, 50);
-				
-				for (int i = 0; i < buttons.length-1; i++) {
-					g.setColor(Color.white);
-					g.texture(buttons[i], bild[i], true);
+		}
+
+	}
+
+
+	public void init(GameContainer container) throws SlickException {
+
+		hoeheBild = container.getHeight();
+		xButton = container.getWidth() / 2 - breiteButton/2;
+
+
+		hintergrundbild = new Image("res/hintergrundbild.png");
+		hintergrundbild = hintergrundbild.getScaledCopy((int)((int) hoeheBild/0.76), hoeheBild ); //breite ergibt sich aus dem Seitenverhältniss
+
+		hintergrundmusik = new Sound("res/hintergrundmusik.ogg");
+		hintergrundmusik.play(-3, 2);
+		click = new Sound("res/klick.wav");
+
+
+		Rectangle start = new Rectangle(xButton, 100, breiteButton, hoeheButton);
+		Rectangle beenden = new Rectangle(xButton, 200, breiteButton, hoeheButton);
+		Circle kreis = new Circle(container.getWidth()-40, 40, 20);
+
+		buttons = new Shape[3];
+		buttons[0]= start;
+		buttons[1]= beenden;
+		buttons[2] = kreis;
+
+		bild = new Image[buttons.length];
+		bild[0] = new Image("res/start.png");
+		bild[1] = new Image("res/ende.png");   //hatte kein Exit Button
+		bild[2] = new Image("res/stop.png");
+
+
+
+	}
+
+	public int update(GameContainer container, int delta, Shape hitboxMaus) {
+
+		for (int i = 0; i < buttons.length; i++) {
+
+			if(hitboxMaus.intersects(buttons[i]) && container.getInput().isMouseButtonDown(0)){
+
+				if(i == 0 && status == 0){
+					click.play(-4, 2);
+					hintergrundmusik.stop();
+					status = 1;
 				}
+				if(i == 1 && status == 0){
+					click.play(-4, 2);
+
+					container.exit();
+				}
+				if(i == 2 && status == 1){
+					click.play(-4, 2);
+					hintergrundmusik.play(-3, 2);
+
+					status = 0;
+
+				}	
 			}
-			
-			
-			
 		}
 
-		
-		public void init(GameContainer container) throws SlickException {
-			
-			x = container.getWidth() / 2 - breite/2;
-			
-			
-			Rectangle start = new Rectangle(x, 100, breite, hoehe);
-			Rectangle beenden = new Rectangle(x, 200, breite, hoehe);
-			Circle kreis = new Circle(container.getWidth()-40, 40, 20);
-
-			buttons = new Shape[3];
-			
-			buttons[0]= start;
-			buttons[1]= beenden;
-			buttons[2] = kreis;
-			
-			bild = new Image[buttons.length];
-			
-			bild[0] = new Image("res/play.png");
-			bild[1] = new Image("res/play.png");   //hatte kein Exit Button
-			bild[2] = new Image("res/stop.png");
-		}
-
-		public int getX() {
-			return x;
-		}
+		return status;
+	}
 
 
-		public void setX(int x) {
-			this.x = x;
-		}
+	public int getX() {
+		return xButton;
+	}
 
 
-		public int getHoehe() {
-			return hoehe;
-		}
+	public void setX(int x) {
+		this.xButton = x;
+	}
 
 
-		public void setHoehe(int hoehe) {
-			this.hoehe = hoehe;
-		}
+	public int getHoehe() {
+		return hoeheButton;
+	}
 
 
-		public int getBreite() {
-			return breite;
-		}
+	public void setHoehe(int hoehe) {
+		this.hoeheButton = hoehe;
+	}
 
 
-		public void setBreite(int breite) {
-			this.breite = breite;
-		}
+	public int getBreite() {
+		return breiteButton;
+	}
 
 
-		public Shape[] getMainmenu() {
-			return buttons;
-		}
+	public void setBreite(int breite) {
+		this.breiteButton = breite;
+	}
 
 
-		public void setMainmenu(Shape[] mainmenu) {
-			this.buttons = mainmenu;
-		}
+	public Shape[] getMainmenu() {
+		return buttons;
+	}
 
 
-		public int getStatus() {
-			return status;
-		}
+	public void setMainmenu(Shape[] mainmenu) {
+		this.buttons = mainmenu;
+	}
 
 
-		public void setStatus(int status) {
-			this.status = status;
-		}
+	public int getStatus() {
+		return status;
+	}
 
 
-		public Shape[] getButtons() {
-			return buttons;
-		}
+	public void setStatus(int status) {
+		this.status = status;
+	}
 
 
-		public void setButtons(Shape[] buttons) {
-			this.buttons = buttons;
-		}
+	public Shape[] getButtons() {
+		return buttons;
+	}
+
+
+	public void setButtons(Shape[] buttons) {
+		this.buttons = buttons;
+	}
 
 
 
--- Projekt Spieleentwickung/src/Main.java
+++ Projekt Spieleentwickung/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 	
 	public static void main(String args[]){
 		Kontrolle kontrolle = new Kontrolle("Test");
-		kontrolle.spielStarten();
+		kontrolle.spielErstellen();
 	}
 
 }

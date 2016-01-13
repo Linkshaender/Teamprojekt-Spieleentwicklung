9c4bf18c7cc03a1a0b57264b68c6855263871999
BuildTools <unconfigured@null.spigotmc.org>
2016-01-13 18:49:09 +0100

    Testspielwelt und Heldenbewegung (ohne Kollision)
    Welt erstellt mit TILED (TileMap). Für Kollisionen muss im untersten
    Layer ("blocked") mit rotem Tile aus "blocked.png" gemalt werden.
    
    Der InputHandler übernimmt (den ersten Teil) der Steuerung das Helden
    (ruft dessen Methoden auf).

--- Projekt Spieleentwickung/src/Held.java
+++ Projekt Spieleentwickung/src/Held.java
@@ -1,7 +1,33 @@
+import org.newdawn.slick.geom.Shape;
 
-public class Held{
+
+public class Held {
+
+
+	private Shape placeHolderShape = null;
 	
-	//öaksnjjbsdinsavds
+	//xDir = 1 -> bewegung "nach rechts". xDir = -1 -> "nach links. yDir = -1 -> "nach oben". yDir = 1 -> "nach unten".
+	public void move(int xDir, int yDir) {	
+		//TODO: Collision detection. Am besten in anderer Klasse. (Methodenaufruf)
+		placeHolderShape.setCenterX(placeHolderShape.getCenterX()+xDir);
+		placeHolderShape.setCenterY(placeHolderShape.getCenterY()+yDir);
+	}
+	
+	
+	public void attack() {
+		
+	}
+	
+	
+	
+
+	public Shape getPlaceHolderShape() {
+		return placeHolderShape;
+	}
+
+	public void setPlaceHolderShape(Shape placeHolderShape) {
+		this.placeHolderShape = placeHolderShape;
+	}
 	
 
 }

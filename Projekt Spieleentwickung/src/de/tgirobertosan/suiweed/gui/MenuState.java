26eaf638691907c3aa7c5e0d6a633a631377f976
Farid <houseraidar@gmail.com>
2016-02-14 13:00:25 +0100

    -Umstrukturierung des Menüs -Verändern des Buttons beim Überfahren mit der Maus und beim Klicken (momentan noch default Bilder)

--- Projekt Spieleentwickung/src/de/tgirobertosan/suiweed/gui/MenuState.java
+++ Projekt Spieleentwickung/src/de/tgirobertosan/suiweed/gui/MenuState.java
@@ -1,109 +1,112 @@
 package de.tgirobertosan.suiweed.gui;
 
+import java.util.ArrayList;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Sound;
-import org.newdawn.slick.geom.Circle;
 import org.newdawn.slick.geom.Rectangle;
-import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.gui.MouseOverArea;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 
+
 public class MenuState extends BasicGameState{
-	
-	
+
+
 	private int id = 0;
 
 	private int xButton;
 	private int hoeheButton = 50;
 	private int breiteButton = 120;
-	
-	private Image[] bild;
 
 	private Image hintergrundbild;
-	private int hoeheBild;
 
 	private Sound hintergrundmusik;
 	private Sound click;
-	private Shape[] buttons;
 
-	private Shape hitboxMaus;
+	private ArrayList<MouseOverArea> mouseOverArea = new ArrayList<MouseOverArea>();
+
+	private boolean mouseClicked = false;
+
 
 	@Override
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {
-	
-		hoeheBild = container.getHeight();
+
 		xButton = container.getWidth() / 2 - breiteButton/2;
 
-		
+
 		hintergrundbild = new Image("res/gui/images/hintergrundbild.png");
-		hintergrundbild = hintergrundbild.getScaledCopy((int)((int) hoeheBild/0.76), hoeheBild ); //breite ergibt sich aus dem Seitenverhältniss
+		hintergrundbild = hintergrundbild.getScaledCopy(container.getWidth(), container.getHeight() ); //breite ergibt sich aus dem Seitenverhältniss
 
 		hintergrundmusik = new Sound("res/gui/audio/hintergrundmusik.ogg");
-		hintergrundmusik.play();
+
 		click = new Sound("res/gui/audio/klick.wav");
 
 
-		Rectangle start = new Rectangle(xButton, 100, breiteButton, hoeheButton);
-		Rectangle beenden = new Rectangle(xButton, 200, breiteButton, hoeheButton);
-		
+		mouseOverArea.add(0,new MouseOverArea(container, new Image("res/gui/images/start.png"), new Rectangle(xButton, 100, breiteButton, hoeheButton)));
+		mouseOverArea.get(0).setMouseOverImage(new Image("res/gui/images/ende.png"));
+		mouseOverArea.get(0).setMouseDownImage(new Image("res/gui/images/start.png"));
 
-		buttons = new Shape[2];
-		buttons[0]= start;
-		buttons[1]= beenden;
-		
-		bild = new Image[buttons.length];
-		bild[0] = new Image("res/gui/images/start.png");
-		bild[1] = new Image("res/gui/images/ende.png");   
-		
-		hitboxMaus = new Circle(0, 0, 2);
+
+		mouseOverArea.add(1,new MouseOverArea(container, new Image("res/gui/images/ende.png"), new Rectangle(xButton, 200, breiteButton, hoeheButton)));
+		mouseOverArea.get(1).setMouseOverImage(new Image("res/gui/images/start.png"));
+		mouseOverArea.get(1).setMouseDownImage(new Image("res/gui/images/ende.png"));
+
+
+		hintergrundmusik.play();
+
 	}
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
-		
+
 		hintergrundbild.drawCentered(container.getWidth()/2, container.getHeight()/2);
 
 		g.setColor(Color.red);
 		g.drawString("Spielmenü", xButton, 50);
 
-		for (int i = 0; i < buttons.length; i++) {
-			g.setColor(Color.white);
-			g.texture(buttons[i], bild[i], true);
+
+		for (int i = 0; i < mouseOverArea.size(); i++) {
+
+			mouseOverArea.get(i).render(container, g);
+
 		}
 
-		
+
+
 	}
 
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
-		
-		hitboxMaus.setCenterX(container.getInput().getMouseX());
-		hitboxMaus.setCenterY(container.getInput().getMouseY());
-		
-		for (int i = 0; i < buttons.length; i++) {
 
-			if(hitboxMaus.intersects(buttons[i]) && container.getInput().isMouseButtonDown(0)){
-
-				
-				if(i == 0){
-					click.play();
-					hintergrundmusik.stop();
-					game.enterState(1);
-				}
-				if(i == 1 ){
-					click.play();
-
-					container.exit();
-				}
-			}
+		if(container.getInput().isMouseButtonDown(0)) {
+			mouseClicked = true;
 		}
-		
-	
+
+		if(!container.getInput().isMouseButtonDown(0) && mouseClicked){
+
+
+			mouseClicked = false;
+
+
+			if(mouseOverArea.get(0).isMouseOver()){
+
+				game.enterState(1);
+
+			}
+
+			if(mouseOverArea.get(1).isMouseOver()){
+
+				container.exit();
+
+			}
+
+
+		}
 
 
 	}
@@ -115,5 +118,5 @@ public class MenuState extends BasicGameState{
 		return id;
 	}
 
-	
+
 }
